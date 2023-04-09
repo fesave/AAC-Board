498b7dcc5801c76357a9ab81d423e368858e921d
Carlos Jimenez Sanchez <cjimenezsanchez1@gmail.com>
2023-04-09 20:05:27 +0200

    fix format

--- app/src/main/java/com/architectcoders/aacboard/ui/newDashboard/NewDashboardFragment.kt
+++ app/src/main/java/com/architectcoders/aacboard/ui/newDashboard/NewDashboardFragment.kt
@@ -3,5 +3,4 @@ package com.architectcoders.aacboard.ui.newDashboard
 import androidx.fragment.app.Fragment
 import com.architectcoders.aacboard.R
 
-class NewDashboardFragment: Fragment(R.layout.fragment_new_dashboard) {
-}
+class NewDashboardFragment: Fragment(R.layout.fragment_new_dashboard)
