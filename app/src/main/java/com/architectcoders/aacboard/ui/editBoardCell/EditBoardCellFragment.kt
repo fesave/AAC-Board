478862bdd1277dbd99cb2259089ecee73618c107
Carlos Jimenez Sanchez <cjimenezsanchez1@gmail.com>
2023-04-09 18:42:50 +0200

    added jetpack navigation library and navigation graph

---
+++ app/src/main/java/com/architectcoders/aacboard/ui/editBoardCell/EditBoardCellFragment.kt
@@ -0,0 +1,7 @@
+package com.architectcoders.aacboard.ui.editBoardCell
+
+import androidx.fragment.app.Fragment
+import com.architectcoders.aacboard.R
+
+class EditBoardCellFragment: Fragment(R.layout.fragment_edit_board_cell) {
+}
---
+++ app/src/main/java/com/architectcoders/aacboard/ui/editDashboard/EditDashboardFragment.kt
@@ -0,0 +1,6 @@
+package com.architectcoders.aacboard.ui.editDashboard
+
+import androidx.fragment.app.Fragment
+import com.architectcoders.aacboard.R
+
+class EditDashboardFragment: Fragment(R.layout.fragment_edit_dashboard)
