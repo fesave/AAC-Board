9929196c765b2a64bfd3e2e0371f766865127630
Fernando Sanz <49989790+fesave@users.noreply.github.com>
2023-04-10 16:39:37 +0200

    Create screen

--- app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogram.kt
+++ app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogram.kt
@@ -1,6 +1,5 @@
 package com.architectcoders.aacboard.ui.editPictogram
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,18 +9,10 @@ import com.architectcoders.aacboard.R
 
 class EditPictogram : Fragment() {
 
-    private lateinit var viewModel: EditPictogramViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_edit_pictogram, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this)[EditPictogramViewModel::class.java]
-    }
-
 }
--- app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogramViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.architectcoders.aacboard.ui.editPictogram
-
-import androidx.lifecycle.ViewModel
-
-class EditPictogramViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
