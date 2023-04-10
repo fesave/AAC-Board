33620acc9985a35afb0f2f3aeb158e471642c422
Fernando Sanz <49989790+fesave@users.noreply.github.com>
2023-04-10 16:32:33 +0200

    Create screen

---
+++ app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogram.kt
@@ -0,0 +1,27 @@
+package com.architectcoders.aacboard.ui.editPictogram
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.architectcoders.aacboard.R
+
+class EditPictogram : Fragment() {
+
+    private lateinit var viewModel: EditPictogramViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_pictogram, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[EditPictogramViewModel::class.java]
+    }
+
+}
---
+++ app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogramViewModel.kt
@@ -0,0 +1,5 @@
+package com.architectcoders.aacboard.ui.editPictogram
+
+import androidx.lifecycle.ViewModel
+
+class EditPictogramViewModel : ViewModel() {}
