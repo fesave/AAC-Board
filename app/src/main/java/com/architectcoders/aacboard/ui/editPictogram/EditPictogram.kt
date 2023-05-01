7b47895e927359816766ec4bba570dd0275517d6
Fernando Sanz <49989790+fesave@users.noreply.github.com>
2023-05-01 09:49:43 +0200

    Configure Toolbar

--- app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogram.kt
+++ app/src/main/java/com/architectcoders/aacboard/ui/editPictogram/EditPictogram.kt
@@ -1,17 +1,18 @@
 package com.architectcoders.aacboard.ui.editPictogram
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.architectcoders.aacboard.R
 
 class EditPictogram : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_edit_pictogram, container, false)
     }
